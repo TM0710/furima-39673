ef5983d8a014df67255bb3d881f631e62e0927af
TM0710 <t_manato@icloud.com>
2023-08-19 08:54:11 +0900

    商品出品機能の実装(ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :region
+  belongs_to :shipping_day
 end
---
+++ app/models/shipping_day.rb
@@ -0,0 +1,12 @@
+class ShippingDay < ActiveHash::Base
+  self.data = [
+    { id: 1, take_days: '---' },
+    { id: 2, take_days: '1~2で発送' },
+    { id: 3, take_days: '2~3で発送' },
+    { id: 4, take_days: '4~7で発送' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+
+  end
