24ac80d0c6d4ef27ddfde19ac656fd0d2638b25d
TM0710 <t_manato@icloud.com>
2023-08-18 11:24:19 +0900

    userモデル　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  # validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
-  # validates :nickname,        presence: true
-  # validates :last_name,       presence: true
-  # validates :first_name,      presence: true
-  # validates :last_name_kana,  presence: true
-  # validates :first_name_kana, presence: true
-  # validates :birthday,        presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/.freeze
+  VALID_NAME_REGEX = /\A[一-龥ぁ-ん]/.freeze
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters" }
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters" }
+    validates :last_name_kana,  format: { with: VALID_NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters" }
+    validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters" }
+  end
 end
