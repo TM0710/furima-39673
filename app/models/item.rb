7cc301225761963c96b2216f5e40381e0311121f
TM0710 <t_manato@icloud.com>
2023-08-21 18:34:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_day
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image, :name, :description, :price
+  end
+  
+  validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range" }, numericality:{ with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+
+  with_options numericality: { other_than: 1, message: "can't be blank" }  do
+    validates :category_id, :condition_id, :charge_id, :region_id, :shipping_day_id
+  end
 end
