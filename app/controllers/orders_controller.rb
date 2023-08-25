d987dc386e69aca83af259feb28bb75ec8b7927f
TM0710 <t_manato@icloud.com>
2023-08-25 16:27:54 +0900

    クレジットカード決済の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,39 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def order_params
+    params.require(:order).permit().merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
