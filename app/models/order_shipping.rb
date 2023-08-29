d02387b97e2978e4fb0bb489f185c2fddea47301
TM0710 <t_manato@icloud.com>
2023-08-29 18:03:33 +0900

    バリュー追記

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -15,9 +15,9 @@ class OrderShipping
   end
 
   def save
-    order = Order.create(item_id:, user_id:)
+    order = Order.create(item_id: item_id, user_id: user_id)
 
-    Shipping.create(postal_code:, region_id:, city:, street:, building:, phone:,
+    Shipping.create(postal_code: postal_code, region_id: region_id, city: city, street: street, building: building, phone: phone,
                     order_id: order.id)
   end
 end
