3efda306250a8d8a6ce067b379b3225e640f85ad
TM0710 <t_manato@icloud.com>
2023-08-30 11:06:10 +0900

    発送日までの日数に「日」を追記

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,9 +1,9 @@
 class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 1, take_days: '---' },
-    { id: 2, take_days: '1~2で発送' },
-    { id: 3, take_days: '2~3で発送' },
-    { id: 4, take_days: '4~7で発送' }
+    { id: 2, take_days: '1~2日で発送' },
+    { id: 3, take_days: '2~3日で発送' },
+    { id: 4, take_days: '4~7日で発送' }
   ]
 
   include ActiveHash::Associations
